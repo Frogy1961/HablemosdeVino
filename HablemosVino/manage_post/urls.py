b9f2ec8036e04881559e6bd304a2716240203dc0
Frogy1961 <mantenimiento.total.proyectos@gmail.com>
2023-09-20 17:54:29 -0600

    ligas menu avanzadas 200923

--- HablemosVino/manage_post/urls.py
+++ HablemosVino/manage_post/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('categories/', views.ListAllCategoriesView.as_view(), name='all_categories'),
     path('articles/<slug:slug>', views.ShowPostDetailView.as_view(), name='post'),
     path('indice/', views.IndiceView.as_view(), name='indice'),
+    path('category/vinos', views.CategoryDetailView.as_view(), name='vinos'),
+    path('category/lugares-compartidos', views.CategoryDetailView.as_view(), name='lugares'),
+    path('category/maridajes', views.CategoryDetailView.as_view(), name='maridajes'),
 ]
